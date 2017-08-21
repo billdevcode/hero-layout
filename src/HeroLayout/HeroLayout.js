5f74323b2acf244799328e38c55d46bae3e63ca6
Bill Deng <billdevcode@gmail.com>
2017-08-20 20:13:43 -0700

    Browser test and fix

--- src/HeroLayout/HeroLayout.js
+++ src/HeroLayout/HeroLayout.js
@@ -1,28 +1,21 @@
-import React from 'react';
+import React from 'react'
+import Header from './components/Header'
 import Hero1 from './Hero/Hero1'
 import Hero2 from './Hero/Hero2'
 import Hero3 from './Hero/Hero3'
+import LearnMoreButton from './components/LearnMoreButton'
 import './HeroLayout.css'
 
 const HeroLayout = () => {
     return (
         <div className='HeroLayout'>
-            <header className='Hero-header'>
-                <h1>Dores André</h1>
-                <p>The Spanish Ballerina Pirouetting Her Way to Stardom</p>
-            </header>
+            <Header />
             <div className='Hero-wrapper'>
                 <Hero2 />
                 <Hero1 />
                 <Hero3 />
             </div>
-            <div className='Hero-showMoreBtn'>
-                Learn More
-                <span className='Hero-btn-topLeft'></span>
-                <span className='Hero-btn-topRight'></span>
-                <span className='Hero-btn-bottomLeft'></span>
-                <span className='Hero-btn-bottomRight'></span>
-            </div>
+            <LearnMoreButton />
         </div>
     )
 }
---
+++ src/HeroLayout/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className='Hero-header'>
+            <h1>Dores André</h1>
+            <p>The Spanish Ballerina Pirouetting Her Way to Stardom</p>
+        </header>
+    )
+}
+
+export default Header
